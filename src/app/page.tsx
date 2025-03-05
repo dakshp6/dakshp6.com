d34d36443d39127729431eaa0ef58169f150435b
dakshp6 <dakshpatel6@gmail.com>
2025-03-05 01:44:51 -0500

    Adding responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,80 +10,39 @@ export default function Home() {
 
   return (
   <div>
-    <section id = "body" className="flex flex-col items-center p-10 m-10 overflow-hidden">
+    <section id = "body" className="border flex flex-col items-center gap-y-6 overflow-hidden p-4">
       
-      <section id="Solari" className="flex-1 flex justify-center p-10 w-full">
+      <section id="Space" className=""/>
+
+      <section id="Solari" className="border flex-1 flex justify-center w-full p-4 lg:p-8 xl:p-10">
         
-        <div className="p-4">
+        <div className="border">
           <Display/>
         </div>
       </section>
 
-      <section id="content" className="flex-1 flex flex-col gap-y-4 p-10">
+      <section id="content" className="border flex-1 flex flex-col gap-y-8 justify-center items-center p-4 lg:p-8 xl:p-10  w-full">
 
-        <div className="p-4 flex flex-col lg:flex-row gap-x-4 items-center">
+          <div className="border text-sm md:text-lg lg:text-xl xl:text-2xl p-4 lg:p-8 xl:p-12">
+            Hey there! Welcome to my little corner of the internet. I&apos;m Daksh, and this is my digital lounge. 
+            A space where you&apos;ll soon be able to enjoy some music and venture into my world. 
+            Think of this site as part portfolio and part playground, where I can show off my work and my personality. If you want to connect with me, check out the links below! :)
+          </div>
 
-{
-/*
-        <div className="border border-green-500 place-content-center">
-          {<Image src={daksh} alt={""} className="bg-zinc-100 rounded-full min-w-64 sm:w-80 md:w-80 lg:w-auto xl:w-96" ></Image>}
-        </div>
-*/
-}
+          <div className="border flex flex-row gap-x-8">
 
-          <div className="flex flex-col m-10 lg:m-0 gap-y-12 p-2">
-
-            <div className="">
-              Hi! Welcome to my lounge. I&apos;m Daksh and this is my digital corner where soon you&apos;ll be able to listen to music and have some fun getting to know me :) . 
-              This website will become a home for my portfolio and also a way for me to show off a bit of my personality! Check out the links below if you want to connect with me!
-            </div>
-
-            <div className="flex flex-row gap-x-8 justify-center">
-              <Button variant="outline" asChild className="h-12">
-                <Link href={linkedin ?? "/"} rel="noopener noreferrer" target="_blank">LinkedIn</Link>
-              </Button>
-              <Button variant="outline" asChild className="h-12">
+            <Button variant="outline" asChild className="h-12">
+              <Link href={linkedin ?? "/"} rel="noopener noreferrer" target="_blank">LinkedIn</Link>
+            </Button>
+            
+            <Button variant="outline" asChild className="h-12">
               <Link href={github ?? "/"} rel="noopener noreferrer" target="_blank">GitHub</Link>
-              </Button>
-            </div>
-          </div>
-        </div>
-      </section>
-{
-
-/*
-
-      <section className="flex-1 mt-10 p-10">
-
-          <div className="flex items-center m-10 p-8">
+            </Button>
+          
           </div>
 
       </section>
-
-
-      <section className="flex-1 mt-10 p-10">
-
-        <div className="flex m-10 p-8">
-        </div>
-
-      </section>
-
-
-      <section className="flex-1 mt-10 p-10">
-
-        <div className="flex m-10 p-8">
-        </div>
-
-      </section>
-*/
-}
-      </section>
-{
-/*
-    <footer className="fixed bottom-0">
-    </footer>
-*/
-}
+    </section>
   </div>
   );
 }
