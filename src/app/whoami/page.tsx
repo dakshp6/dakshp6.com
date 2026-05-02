18ca65e9f805e711d0ca57e220431a00f2876c51
dakshp6 <dakshpatel6@gmail.com>
2026-05-01 23:42:02 -0400

    Reverting changes

--- src/app/whoami/page.tsx
+++ src/app/whoami/page.tsx
@@ -1,15 +1,4 @@
-import Image from "next/image";
-
-
 export default function Whoami()
 {
-
-    return (
-        <>
-
-        <section className="w-full p-4 flex flex-col items-center gap-y-6 overflow-hidden">
-        </section>
-
-        </>
-      );
+    return <h1>whoami</h1>
 }
